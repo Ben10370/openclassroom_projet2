0bea18b5d40947fe6ea3531297df4333fa676e62
Benoit BONNIVARD <benoit.bonnivard776@gmail.com>
2023-04-04 09:50:36 +0200

    rajout de code qui crée un fichier csv avec une boucle liste cle et liste valeur depuis un dictionnaire pour les entêtes des colones

--- scrapbook_data.py
+++ scrapbook_data.py
@@ -22,6 +22,22 @@ def scrap_book(book_url):
     book_data["review_rating"] = soup.find_all("td")[6].text
     cover_url = soup.img["src"][5:]
     book_data["img_url"] = f"https://books.toscrape.com{cover_url}"
+    
+    with open(f'{book_data["title"]}.csv', mode="w", newline="") as f:
+        writer = csv.writer(f)
+        
+        liste_cle = []
+        for cle in book_data.keys():
+            liste_cle.append(cle)
+        print(liste_cle)
+        writer.writerow(liste_cle)
+        
+        liste_valeur = []
+        for valeur in book_data.values():
+            liste_valeur.append(valeur)
+        print(liste_valeur)
+        writer.writerow(liste_valeur)
+    
     return book_data
 
 print(scrap_book(book_url))
